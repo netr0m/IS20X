c54e5b9c49028cf7199d461d07ec3b0cff76df60
mortea15 <mortea15@student.uia.no>
2016-09-12 12:26:54 +0200

    Update

--- IS20X/src/is20x/User.java
+++ IS20X/src/is20x/User.java
@@ -1,34 +1,15 @@
 package is20x;
  
-import java.util.HashMap;
-import java.util.Map;
- 
 public class User {
- 
-    private static final Map<String, User> USERS = new HashMap<String, User>();
- 
-    public static User of(String id) {
-        User user = USERS.get(id);
-        if (user == null) {
-            user = new User(id);
-            USERS.put(id, user);
-        }
-        return user;
+    private int studentID;
+    private String name;
+    private String username;
+    private String password;
+    private String email;
+    
+    public int getID() {
+        return studentID;
     }
- 
-    private User(String id) {
-        this.id = id;
-    }
-    private String id;
- 
-    public String getId() {
-        return id;
-    }
-    private String name = "";
-    private String email = "";
-    private boolean teacher = false;
-    private boolean admin = false;
-    private String password = "1234";
     
     /**
      */
@@ -56,35 +37,20 @@ public class User {
  
     /**
      */
-    public boolean isAdmin() {
-        return admin;
+    public String getUsername() {
+        return username;
     }
  
     /**
      */
-    public void setAdmin(boolean admin) {
-        if (this.isAdmin()) {
-            this.admin = admin;
-        }
+    public void setUsername(String uname) {
+        this.username = uname;
     }
-    
+
     /**
      */
-    public boolean isTeacher() {
-        return teacher;
-    }
- 
-    /**
-     */
-    public void setTeacher(boolean teacher) {
-        if (this.isAdmin()) {
-            this.teacher = teacher;
-        }
-    }
 private String oldPassword;
 private String newPassword;
-    /**
-     */
     public void setPassword(String oldPassword, String newPassword) {
         if (this.oldPassword == this.password) {
         this.password = newPassword;
