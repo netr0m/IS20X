e4043b8186373d126fd3526f5072e55191279fbb
Morten Kregnes Amundsen <mortea15@uia.no>
2016-11-03 01:55:03 +0100

    A bean, a cup of coffee, and a client. Such a lovely meeting at a library cafe.

--- slit/slit-client/src/java/slitclient/LoginController.java
+++ slit/slit-client/src/java/slitclient/LoginController.java
@@ -6,20 +6,13 @@
 package slitclient;
 
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ResourceBundle;
-import java.util.function.Consumer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TextInputDialog;
 import auth.LoginAuthenticator;
 import javax.naming.Context;
 import javax.naming.InitialContext;
