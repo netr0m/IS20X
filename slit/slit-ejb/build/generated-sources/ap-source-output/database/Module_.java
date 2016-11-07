fc3076b0f501026060c50557e57e55372afc6428
mortea15 <mortea15@student.uia.no>
2016-11-07 14:26:42 +0100

    Update 412563

--- slit/slit-ejb/build/generated-sources/ap-source-output/database/Module_.java
+++ slit/slit-ejb/build/generated-sources/ap-source-output/database/Module_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-11-03T20:56:30")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-11-04T12:08:28")
 @StaticMetamodel(Module.class)
 public class Module_ { 
 
--- slit/slit-ejb/build/generated-sources/ap-source-output/database/ModuledeliveryPK_.java
+++ slit/slit-ejb/build/generated-sources/ap-source-output/database/ModuledeliveryPK_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-11-03T20:56:30")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-11-04T12:08:28")
 @StaticMetamodel(ModuledeliveryPK.class)
 public class ModuledeliveryPK_ { 
 
